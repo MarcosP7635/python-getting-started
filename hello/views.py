2cab5a96b6b5c60acfb1747e28790a4565f6cc99
MarcosP7635 <marcos.aaron.perez@gmail.com>
2022-08-22 13:39:45 -0700

    trying a link to the plot

--- hello/views.py
+++ hello/views.py
@@ -8,6 +8,8 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
+def plot(request):
+    return render(request, "neutrons_U238.html")
 
 def db(request):
 
